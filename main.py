5fc5389d6f0c247b5bb9ee758548c750c6509f1a
filipeferreirakk <filipegomesfrr@gmail.com>
2026-05-06 00:54:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -75,3 +75,39 @@ class Biblioteca:
         print("ID não encontrado.")
 
 
+class NodoFila:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
+
+
+class Fila:
+    def __init__(self):
+        self.inicio = None
+        self.fim = None
+        self.tamanho = 0
+
+    def enqueue(self, musica):
+        novo = NodoFila(musica)
+        if self.fim is None:
+            self.inicio = novo
+            self.fim = novo
+        else:
+            self.fim.proximo = novo
+            self.fim = novo
+        self.tamanho += 1
+
+    def dequeue(self):
+        if self.inicio is None:
+            return None
+        musica = self.inicio.musica
+        self.inicio = self.inicio.proximo
+        if self.inicio is None:
+            self.fim = None
+        self.tamanho -= 1
+        return musica
+
+    def vazia(self):
+        return self.inicio is None
+
+
